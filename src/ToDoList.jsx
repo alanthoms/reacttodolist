e4f7f59aecd06ac41257258c02ff2459848737a3
Alan Thomas <alanthomas171@gmail.com>
2025-06-27 14:43:58 +0100

    Add task add and delete

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -13,11 +13,24 @@ function ToDoList(){
     }
 
     function addTask(){
+        //updater function
+        //setTasks is state updater functoin for tasks array from Use State
+        //passing in an updater function
+        //idk t is latest version of task list and spreading by ... helps update immutably 
 
+        //simple if that removes whitespace and checks for empty string
+        if(newTask.trim() !==""){
+            setTasks(t => [...t,newTask])
+            setNewTask("");
+        }
     }
 
     function deleteTask(index){
 
+        //filter with arrow function, if index matches i, filtered out
+        // we keep i that doesnt equal index
+        const updatedTasks = tasks.filter((_,i) => i !== index);
+        setTasks(updatedTasks);
     }
 
     function moveTaskUp(index){
