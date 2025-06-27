66d31693ccddf201cec7d8fe92c3b47eee6de094
Alan Thomas <alanthomas171@gmail.com>
2025-06-27 13:45:51 +0100

    Add html and css

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -2,10 +2,13 @@ import React, {useState} from 'react'
 
 function ToDoList(){
 
-    const [tasks, setTasks] = useState([]);
+    const [tasks, setTasks] = useState(["Eat Breakfast", "Shower","Watch a film"]);
     const [newTask, setNewTask] = useState("");
 
+    //event handler, function uses setter for newTask to access event parameter target's value
+    // enables visual change of textbox value
     function handleInputChange(event){
+        setNewTask(event.target.value);
 
     }
 
@@ -33,8 +36,41 @@ function ToDoList(){
                     type="text"
                     placeholder="Enter a task..."
                     value={newTask}
-                    onChange={handleInputChange}/>
+                    onChange={handleInputChange}
+                />
+                <button
+                    className="add-button"
+                    onClick={addTask}
+                    >
+                    Add   
+
+                </button>
             </div>
+            <ol>
+                {tasks.map((taskElement,index)=>
+                    <li key={index}><span className ="text">{taskElement}</span>
+                    <button 
+                    className='delete-button' 
+                    //arrow function so it does not call function immediately
+                    onClick={() => deleteTask(index)}>
+                        Delete
+                    </button>
+
+                    <button 
+                    className='move-button' 
+                    //arrow function so it does not call function immediately
+                    onClick={() => moveTaskUp(index)}>
+                        ⬆️ 
+                    </button>
+                    <button 
+                    className='move-button' 
+                    //arrow function so it does not call function immediately
+                    onClick={() => moveTaskDown(index)}>
+                        ⬇️
+                    </button>
+                    </li>
+                )}
+            </ol>
         </div>
     );
 }
